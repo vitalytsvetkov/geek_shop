5609cfd8f5b75294da8f6bc82aa59165af2db8e1
vitalytsvetkov <vitalytsvetkov88@gmail.com>
2019-09-08 17:27:47 +0300

    Так как не корректно начала работать без веток - исправил и добавил gitignore

---
+++ mainapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def main(request):
+    return render(request, 'mainapp/main.html')
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+def contacts(request):
+    return render(request, 'mainapp/contacts.html')
+
+# Create your views here.
